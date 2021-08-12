3d55543c142241eaf84061522086e788765c14f6
othree <othree@smartpay.co>
2021-08-12 21:27:53 +0800

    [ch187] Add testGetSessionURL

--- test/index.js
+++ test/index.js
@@ -4,16 +4,17 @@ import test from 'tape';
 import Smartpay from '../build/index.js';
 
 const API_PREFIX = 'https://api.smartpay.co/checkout';
+const CHECKOUT_URL = 'https://checkout.smartpay.co';
 
 const TEST_SECRET_KEY = 'sk_test_albwlejgsekcokfpdmva';
 const TEST_PUBLIC_KEY = 'pk_test_albwlejgsekcokfpdmva';
 
+const FAKE_SESSION = {
+  id: 'cs_live_abcdef12345678',
+};
+
 test('Create Checkout Session', async function testCreateCheckoutSession(t) {
   t.plan(2);
-
-  const fakeSession = {
-    id: 'cs_live_abcdef12345678',
-  };
 
   const scope = nock(API_PREFIX, {
     reqheaders: {
@@ -21,7 +22,7 @@ test('Create Checkout Session', async function testCreateCheckoutSession(t) {
     },
   })
     .post('/sessions')
-    .reply(200, JSON.stringify(fakeSession));
+    .reply(200, JSON.stringify(FAKE_SESSION));
 
   const smartpay = new Smartpay(TEST_SECRET_KEY, {
     publicKey: TEST_PUBLIC_KEY,
@@ -41,6 +42,21 @@ test('Create Checkout Session', async function testCreateCheckoutSession(t) {
   };
   const session = await smartpay.createCheckoutSession(payload);
 
-  t.equal(session.id, fakeSession.id);
+  t.equal(session.id, FAKE_SESSION.id);
   t.ok(scope.isDone());
+});
+
+test('Get Session URL', function testGetSessionURL(t) {
+  t.plan(3);
+
+  const smartpay = new Smartpay(TEST_SECRET_KEY, {
+    publicKey: TEST_PUBLIC_KEY,
+    checkoutURL: CHECKOUT_URL,
+  });
+
+  const sessionURL = smartpay.getSessionURL(FAKE_SESSION);
+
+  t.ok(sessionURL.indexOf(CHECKOUT_URL) === 0);
+  t.ok(sessionURL.indexOf(`key=${TEST_PUBLIC_KEY}`) > 0);
+  t.ok(sessionURL.indexOf(`session=${FAKE_SESSION.id}`) > 0);
 });

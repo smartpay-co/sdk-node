477dde42debf64703e0b6c2fd0236e1c2b321ef3
othree <othree@smartpay.co>
2021-09-24 16:34:20 +0800

    Improve schemas.sh

--- scripts/schemas.sh
+++ scripts/schemas.sh
@@ -3,7 +3,10 @@
 FILES=src/schemas/*.json
 
 for f in $FILES; do
+  echo $f
   OUT=src/schemas/$(basename -s .json $f).ts
   printf '%s %s\n' "export default " "$(cat $f)" > $OUT
   npx prettier --write $OUT
 done
+
+echo "done"
